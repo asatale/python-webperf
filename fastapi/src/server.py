c1e554bc6c4cf7b375fe349e8c6ef336228b9365
Aniruddha Atale <aniruddha.atale@klaviyo.com>
2022-02-07 18:05:04 -0500

    uvicorn config changes

--- fastapi/src/server.py
+++ fastapi/src/server.py
@@ -1,3 +1,4 @@
+import asyncio
 import uvicorn
 import multiprocessing
 from fastapi import FastAPI
@@ -5,6 +6,7 @@ from middleware.delay import DelayMiddleware
 from middleware.cancel import CancelMiddleware
 from api.hello import app as helloApp
 from config import cfg
+from log import logger
 
 app = FastAPI()
 app.mount("/", helloApp)
@@ -17,12 +19,38 @@ def main():
         return (multiprocessing.cpu_count() * 2) + 1
 
     addr, port = cfg.addr.split(":")
-    uvicorn.run(
-        f"{__name__}:app",
+    loop = asyncio.new_event_loop()
+    config = uvicorn.Config(
+        app=app,
         host=addr,
         port=int(port),
         log_level="info",
-        workers=_num_workers())
+        log_config={
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "fmt": '[%(asctime)s] [%(filename)s:%(lineno)d] %(levelname)s: %(message)s',
+                    "use_colors": True,
+                },
+                "access": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "fmt": '[%(asctime)s] [%(filename)s:%(lineno)d] %(levelname)s: %(message)s',
+                    "use_colors": True,
+                },
+            },
+        },
+        limit_concurrency=1000,
+        workers=_num_workers(),
+        loop=loop
+    )
+
+    server = uvicorn.Server(config)
+
+    try:
+        loop.run_until_complete(server.serve())
+    except Exception as e:
+        logger.error(f"Exception {e} in asyncio loop")
 
 
 if __name__ == "__main__":
